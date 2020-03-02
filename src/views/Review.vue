583ff2b2f3cf22fd5976daec78870cc46639145b
filigreti <aolfiligre@gmail.com>
2020-03-02 19:09:24 +0100

    WIP

--- src/views/Review.vue
+++ src/views/Review.vue
@@ -4,33 +4,43 @@
     <div class="grid mt-5 mx-3">
       <div class="card shadow p-3">
         <h2 class="text-center">Reactions</h2>
-        <div class="d-flex justify-content-center align-items-center w-100 h-100 flex-column">
+        <div
+          class="d-flex justify-content-center align-items-center w-100 h-100 flex-column"
+        >
           <img :src="newStore.emoji.imageSample" class="img-fluid pt-2" alt />
-          <h3 class="text-center pt-3">{{newStore.emoji.imagetext}}</h3>
+          <h3 class="text-center pt-3">{{ newStore.emoji.imagetext }}</h3>
         </div>
       </div>
       <div class="card shadow p-3">
         <h2 class="text-center">Title</h2>
-        <div class="d-flex justify-content-center align-items-center w-100 h-100">
-          <h3 class="text-center pt-4">{{newStore.textOne}}</h3>
+        <div
+          class="d-flex justify-content-center align-items-center w-100 h-100"
+        >
+          <h3 class="text-center pt-4">{{ newStore.textOne }}</h3>
         </div>
       </div>
       <div class="card shadow p-3">
         <h2 class="text-center">Article Image</h2>
-        <div class="d-flex justify-content-center align-items-center w-100 h-100 flex-column">
+        <div
+          class="d-flex justify-content-center align-items-center w-100 h-100 flex-column"
+        >
           <img :src="newStore.articleImage" class="img-fluid pt-2" alt />
         </div>
       </div>
       <div class="card shadow p-3">
         <h2 class="text-center">Comment</h2>
-        <div class="d-flex justify-content-center align-items-center w-100 h-100">
-          <h3 class="text-center pt-4">{{newStore.comment}}</h3>
+        <div
+          class="d-flex justify-content-center align-items-center w-100 h-100"
+        >
+          <h3 class="text-center pt-4">{{ newStore.comment }}</h3>
         </div>
       </div>
       <div class="card shadow p-3">
         <h2 class="text-center">Name</h2>
-        <div class="d-flex justify-content-center align-items-center w-100 h-100">
-          <h3 class="text-center pt-4">{{newStore.name}}</h3>
+        <div
+          class="d-flex justify-content-center align-items-center w-100 h-100"
+        >
+          <h3 class="text-center pt-4">{{ newStore.name }}</h3>
         </div>
       </div>
     </div>
@@ -60,9 +70,10 @@ export default {
       let all = this.sendData;
       all.timeline.tracks[0].clips.forEach((key, index) => {
         let k = key.asset.text;
-        if (k.length > 30) {
-          all.timeline.tracks[0].clips[index].asset.text = breakText(k);
-        }
+        // console.log(k);
+        // if (k.length > 30) {
+        //   all.timeline.tracks[0].clips[index].asset.text = breakText(k);
+        // }
       });
       let axiosConfig = {
         headers: {
@@ -84,7 +95,6 @@ export default {
   },
   mounted() {
     this.newStore = this.$store.state.allData;
-    console.log(this.newStore);
     let allData = {
       timeline: {
         soundtrack: {
@@ -92,17 +102,19 @@ export default {
             "https://songsa.s3-us-west-1.amazonaws.com/Ikson+-+Paradise+(Travel+Vlog+Background+Music)+(Free+To+Use+Music).mp3",
           effect: "fadeInFadeOut"
         },
-        background: "#000000",
+
         tracks: [
           {
             clips: [
               {
                 asset: {
-                  type: "title",
-                  text: `${this.newStore.name} is ${this.newStore.emoji.imagetext} about something`,
-                  style: "chunk",
-                  position: "top",
-                  size: "x-large"
+                  type: "html",
+                  text: `<b>Charles</b><br><p> Is happy about Something </p> `,
+                  css: "p { color: #ffffff; } b { color: #ffff00; }",
+                  width: 400,
+                  height: 300,
+                  background: "transparent",
+                  position: "center"
                 },
                 start: 2,
                 length: 3,
@@ -115,10 +127,10 @@ export default {
               {
                 asset: {
                   type: "title",
-                  text: `${this.newStore.textOne}`,
-                  style: "chunkLight",
+                  text: `Buhari is still president for the third time`,
+                  style: "minimal",
                   position: "center",
-                  size: "x-large"
+                  size: "medium"
                 },
                 start: 7,
                 length: 3,
@@ -130,10 +142,10 @@ export default {
               {
                 asset: {
                   type: "title",
-                  text: `${this.newStore.name} Says`,
-                  style: "subtitle",
+                  text: `Charles Says`,
+                  style: "minimal",
                   position: "chunk",
-                  size: "x-large"
+                  size: "medium"
                 },
                 start: 11,
                 length: 2,
@@ -146,24 +158,20 @@ export default {
               {
                 asset: {
                   type: "title",
-                  text: `"${this.newStore.comment}"`,
-                  style: "subtitle",
+                  text: `"How can i still be happy with things,\n  when Buhari is still president"`,
+                  style: "minimal",
                   position: "center",
-                  size: "x-large"
+                  size: "medium"
                 },
                 start: 13,
                 length: 4
               },
               {
                 asset: {
-                  type: "title",
-                  text: "What do you think ?",
-                  style: "chunk",
-                  position: "topLeft",
-                  size: "xx-large",
-                  offset: {
-                    x: -0.68
-                  }
+                  type: "html",
+                  html: "<h3> What do you think </h3>",
+                  css: "h3{ padding:20px,color:#f1c40f;}",
+                  position: "topLeft"
                 },
                 start: 18,
                 length: 3
@@ -175,12 +183,12 @@ export default {
               {
                 asset: {
                   type: "image",
-                  src: `${this.newStore.emoji.imageSample}`
+                  src: `http://www.pngall.com/wp-content/uploads/2016/06/Fearful-Emoji-PNG.png`
                 },
                 start: 0,
                 length: 6,
                 scale: 0.3,
-                position: "bottom"
+                position: "center"
               }
             ]
           },
@@ -189,7 +197,7 @@ export default {
               {
                 asset: {
                   type: "image",
-                  src: `${this.newStore.articleImage}`
+                  src: `https://images.pexels.com/photos/3347244/pexels-photo-3347244.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500`
                 },
                 start: 6,
                 length: 4,
